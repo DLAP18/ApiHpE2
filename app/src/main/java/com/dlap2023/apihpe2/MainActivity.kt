380aa07a0992e8c18a09f91c84aa2d650a7a7737
DLAP18 <dlap-16@hotmail.com>
2023-06-10 21:08:48 -0600

    Commit inicial

---
+++ app/src/main/java/com/dlap2023/apihpe2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.dlap2023.apihpe2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.dlap2023.apihpe2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
